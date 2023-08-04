c57d310fe08afb25a63a45e999e9003d33f2b19d
ahoffskov <36532723+ahoffskov@users.noreply.github.com>
2023-08-04 13:13:30 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,62 +1,23 @@
-// Create web server
+// Create webserver
+// http://localhost:3000/
+// http://localhost:3000/api/comment
+// http://localhost:3000/api/comment/1
 
-// 1. Import Express
+// Import package
 const express = require('express')
-const path = require('path')
-const fs = require('fs')
 const app = express()
+const bodyParser = require('body-parser')
 
-// 2. Setup static folder
-app.use(express.static('public'))
+// Import router
+const comment = require('./routes/comment')
 
-// 3. Setup template engine
-app.set('view engine', 'hbs')
+// Set body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// 4. Setup router
-app.get('/', (req, res) => {
-    const comments = JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/comments.json'), { encoding: 'utf-8' }))
-    res.render('home', {
-        comments: comments
-    })
-})
+// Set router
+app.use('/api/comment', comment)
 
-app.get('/add', (req, res) => {
-    res.render('add')
-})
-
-app.get('/detail/:id', (req, res) => {
-    const comments = JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/comments.json'), { encoding: 'utf-8' }))
-    const comment = comments.find(cmt => cmt.id === req.params.id)
-    res.render('detail', {
-        comment: comment
-    })
-})
-
-app.get('/edit/:id', (req, res) => {
-    const comments = JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/comments.json'), { encoding: 'utf-8' }))
-    const comment = comments.find(cmt => cmt.id === req.params.id)
-    res.render('edit', {
-        comment: comment
-    })
-})
-
-app.get('/delete/:id', (req, res) => {
-    const comments = JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/comments.json'), { encoding: 'utf-8' }))
-    const comment = comments.find(cmt => cmt.id === req.params.id)
-    res.render('delete', {
-        comment: comment
-    })
-})
-
-app.get('/search', (req, res) => {
-    const comments = JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/comments.json'), { encoding: 'utf-8' }))
-    const comment = comments.filter(cmt => cmt.name.toLowerCase().includes(req.query.name.toLowerCase()))
-    res.render('search', {
-        comments: comment
-    })
-})
-
-// 5. Start web server
-app.listen(3000, () => {
-    console.log('Web server running on port 3000')
-})
+// Set port
+const port = 3000
+app.listen(port, () => console.log(`Server running on port ${port}`))
